7b265361f07e582d2377572829d060d97b6fb7be
iredstonne <iredstonne.dev@gmail.com>
2025-05-07 20:32:50 +0200

    Add Unix socket support for PDO

--- app/src/Database/Connection.php
+++ app/src/Database/Connection.php
@@ -8,15 +8,23 @@ class Connection
     public static function getPDO(): \PDO 
     {
         if(!self::$pdo) {
-            $dsn = sprintf(
-                "mysql:host=%s;port=%s;dbname=%s;charset=UTF8",
-                DATABASE_HOST,
-                DATABASE_PORT,
-                DATABASE_NAME
-            );
+            if(defined("DATABASE_SOCKET") && DATABASE_SOCKET && is_readable(DATABASE_SOCKET)) {
+                $dsn = sprintf(
+                    "mysql:unix_socket=%s;dbname=%s;charset=UTF8",
+                    DATABASE_SOCKET,
+                    DATABASE_NAME
+                );
+            } else {
+                $dsn = sprintf(
+                    "mysql:host=%s;port=%s;dbname=%s;charset=UTF8",
+                    DATABASE_HOST,
+                    DATABASE_PORT,
+                    DATABASE_NAME
+                );
+            }
             $options = [
                 \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ
             ];
 
             self::$pdo = new \PDO(
