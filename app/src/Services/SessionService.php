2b04e6fc5bcdcedc7643a56ac35d10ab3f790ee8
iredstonne <iredstonne.dev@gmail.com>
2025-06-02 12:56:34 +0200

    Add the check if client is authenticated to firewall or not

--- app/src/Services/SessionService.php
+++ app/src/Services/SessionService.php
@@ -10,17 +10,21 @@ use Slim\Exception\HttpException;
 class SessionService
 {
     public static function connect(DeviceModel $device, CouponModel $coupon): bool {
-        if(!Firewall::authenticate($device)) {
-            return false;
-        } 
+        if(!Firewall::isAuthenticated($device)) {
+            if(!Firewall::authenticate($device)) {
+                return false;
+            }
+        }
         SessionModel::connect($device, $coupon);
         return true;
     }
 
     public static function disconnect(DeviceModel $device, SessionModel $session) {
-        if(!Firewall::deauthenticate($device)) {
-            return false;
-        } 
+        if(Firewall::isAuthenticated($device)) {
+            if(!Firewall::deauthenticate($device)) {
+                return false;
+            }
+        }
         $session->disconnect();
         return true;
     }
